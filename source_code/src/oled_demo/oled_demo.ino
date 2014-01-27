529b7afaf1bf0e0358a27426b233a82f9c8a37e3
Harlequin <harlequin@hunt.net.nz>
2014-01-27 23:02:19 +1300

    switch to oledmp library

--- source_code/src/oled_demo/oled_demo.ino
+++ source_code/src/oled_demo/oled_demo.ino
@@ -4,16 +4,13 @@
  */
 
 #include <SPI.h>
-#include <oled256.h>
+#include <oledmp.h>
 
 #include "hackaday.h"
 //#include "jack.h"
 #include "gear.h"
 
-#define LCD_WIDTH 32
-#define LCD_HEIGHT 5
-
-LcdDisplay lcd(17, 16, 10);	// cs, data_command, reset
+OledMP oled(17, 16, 10);	// cs, data_command, reset
 
 void setup()
 {
@@ -25,23 +22,20 @@ void setup()
     SPI.setDataMode(SPI_MODE0);
     SPI.setClockDivider(SPI_CLOCK_DIV2);
 
-    lcd.begin(LCD_WIDTH, LCD_HEIGHT, FONT_NINE_DOT);
-    //lcd.begin(LCD_WIDTH, LCD_HEIGHT, FONT_FIFTEEN_DOT);
-    lcd.setColour(15);
-    lcd.setBackground(0);
-    lcd.setContrast(255);
+    oled.begin(font_CHECKBOOK_14);
 
-    lcd.setCursor(1,0);
+    oled.setColour(15);
+    oled.setBackground(0);
+    oled.setContrast(255);
 
-    lcd.bitmapDraw(0,0, HACKADAY_WIDTH, HACKADAY_HEIGHT, hackaday_image);
-    lcd.bitmapDraw(192,0, GEAR_WIDTH, GEAR_HEIGHT, gear_image);
-    //lcd.bitmapDraw(160,0, JACK_WIDTH, JACK_HEIGHT, jack_image);
-    //lcd.bitmapDraw(160,0,SPHERE_WIDTH,SPHERE_HEIGHT,sphere_image);
-    lcd.setFontHQ(font_CHECKBOOK_14);
-    lcd.setXY(108,4);
-    lcd.print(F("Mooltipass"));
-    lcd.setXY(72,37);
-    lcd.print(F("HACK A DAY"));
+    oled.bitmapDraw(0,0, HACKADAY_WIDTH, HACKADAY_HEIGHT, hackaday_image);
+    oled.bitmapDraw(192,0, GEAR_WIDTH, GEAR_HEIGHT, gear_image);
+    //oled.bitmapDraw(160,0, JACK_WIDTH, JACK_HEIGHT, jack_image);
+    //oled.bitmapDraw(160,0,SPHERE_WIDTH,SPHERE_HEIGHT,sphere_image);
+    oled.setXY(108,4);
+    oled.printf(F("Mooltipass"));
+    oled.setXY(72,37);
+    oled.printf(F("HACK A DAY"));
 }
 
 void loop()

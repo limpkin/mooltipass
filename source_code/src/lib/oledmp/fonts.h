fb4338af6abcb9f3ec3de4fa5fe7d3adb0eb9aa1
Harlequin <harlequin@hunt.net.nz>
2014-02-15 13:53:45 +1300

    Add profont fonts

--- source_code/src/lib/oledmp/fonts.h
+++ source_code/src/lib/oledmp/fonts.h
@@ -7,7 +7,10 @@
 // Font selection
 #undef FONT_CHECKBOOK_12
 #define FONT_CHECKBOOK_14
+#undef FONT_PROFONT_10_100DPI
+#undef FONT_PROFONT_10_72DPI
 #define FONT_DEFAULT font_CHECKBOOK_14
+//#define FONT_DEFAULT font_PROFONT_10_100DPI
 
 typedef struct {
     uint8_t width;		// Width of glyph data in pixels
@@ -30,6 +33,12 @@ typedef struct {
 #ifdef FONT_CHECKBOOK_14
 #include "font_checkbook_14.h"
 #endif
+#ifdef FONT_PROFONT_10_100DPI
+#include "font_profont_10_100dpi.h"
+#endif
+#ifdef FONT_PROFONT_10_72DPI
+#include "font_profont_10_72dpi.h"
+#endif
 
 typedef enum {
 #ifdef FONT_CHECKBOOK_12
@@ -37,6 +46,12 @@ typedef enum {
 #endif
 #ifdef FONT_CHECKBOOK_14
     font_CHECKBOOK_14,
+#endif
+#ifdef FONT_PROFONT_10_100DPI
+    font_PROFONT_10_100DPI,
+#endif
+#ifdef FONT_PROFONT_10_72DPI
+    font_PROFONT_10_72DPI,
 #endif
 } font_e;
 

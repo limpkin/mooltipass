3ac5e2e585f8bb34efa154a91d74d6236b2da286
M.A. Borrego <jimjim17@gmail.com>
2014-03-05 17:53:30 +0100

    removed AES unnecessary files

--- source_code/src/AES/aes/aes.h
+++ source_code/src/AES/aes/aes.h
@@ -30,11 +30,7 @@
 #include <stdint.h>
 
 #include "aes_types.h"
-#include "aes128_enc.h"
-#include "aes192_enc.h"
 #include "aes256_enc.h"
-#include "aes128_dec.h"
-#include "aes192_dec.h"
 #include "aes256_dec.h"
 #include "aes_enc.h"
 #include "aes_dec.h"

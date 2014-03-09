14af04d50d16310461aad3ec0c5f2b20c4e24718
M.A. Borrego <jimjim17@gmail.com>
2014-03-09 12:52:57 +0100

    removed aes 128/192 related code

--- source_code/src/AES/aes/aes_keyschedule.h
+++ source_code/src/AES/aes/aes_keyschedule.h
@@ -30,6 +30,7 @@
 #define AES_KEYSCHEDULE_H_
 
 #include "aes_types.h"
+
 /**
  * \brief initialize the keyschedule
  *
@@ -37,29 +38,9 @@
  * and stores it in the context variable
  * \param key       pointer to the key material
  * \param keysize_b length of the key in bits (valid are 128, 192 and 256)
- * \param ctx       pointer to the context where the keyschedule should be stored
+ * \param ctx       pointer to the context where the keyschedule should be stor
  */
 void aes_init(const void* key, uint16_t keysize_b, aes_genctx_t* ctx);
-
-/**
- * \brief initialize the keyschedule for 128 bit key
- *
- * This function computes the keyschedule from a given 128 bit key
- * and stores it in the context variable
- * \param key       pointer to the key material
- * \param ctx       pointer to the context where the keyschedule should be stored
- */
-void aes128_init(const void* key, aes128_ctx_t* ctx);
-
-/**
- * \brief initialize the keyschedule for 192 bit key
- *
- * This function computes the keyschedule from a given 192 bit key
- * and stores it in the context variable
- * \param key       pointer to the key material
- * \param ctx       pointer to the context where the keyschedule should be stored
- */
-void aes192_init(const void* key, aes192_ctx_t* ctx);
 
 /**
  * \brief initialize the keyschedule for 256 bit key

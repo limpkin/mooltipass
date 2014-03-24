44bd2d2ff0d119399085fee859a7cf92a67761fc
Harlequin <harlequin@hunt.net.nz>
2014-03-24 23:47:13 +1300

    fix datatype for y when it can't be negative

--- source_code/src/OLEDMP/oledmp.c
+++ source_code/src/OLEDMP/oledmp.c
@@ -643,7 +643,7 @@ void oledScrollClear(uint8_t options)
     }
     else 
     {
-        for (int8_t y=0; y<OLED_HEIGHT; y++) 
+        for (unt8_t y=0; y<OLED_HEIGHT; y++) 
         {
             oledSetWindow(0, y, OLED_WIDTH-1, y);
             oledWriteCommand(CMD_WRITE_RAM);

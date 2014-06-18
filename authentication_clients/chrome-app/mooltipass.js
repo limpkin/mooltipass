f218c863be3066ab5bd3b6a25bfa2e207c380f69
Mikael Albertsson <mikael.albertsson@gmail.com>
2014-06-18 16:40:05 +0200

    Reworked the GUI and added auto-launch
    Reworked the GUI of the app to be more useful and controllable by the user.
    Changed the app to auto-launch on app install and browser startup.

--- authentication_clients/chrome-app/mooltipass.js
+++ authentication_clients/chrome-app/mooltipass.js
@@ -3,13 +3,12 @@ var mp_product_id = 0x09a0; // 2464
 var device_info = { "vendorId": mp_vendor_id, "productId": mp_product_id };
 
 // Commands that the MP device can send.
+// TODO: Add new commands.
 var CMD_DEBUG   = 0x01
 var CMD_PING    = 0x02
 var CMD_VERSION = 0x03
 
 var mp_device = null;
-var message = document.getElementById("message");
-var requestButton = document.getElementById("requestPermission");
 
 var in_transfer = {
 	direction: 'in',
@@ -17,11 +16,35 @@ var in_transfer = {
 	length: 64
 };
 
+function message(msg) {
+	message_div = document.getElementById("message");
+	message_div.innerHTML += msg;
+}
+
+function messageln(msg) {
+	message(msg + '<br />\n');
+}
+
+function warning(msg) {
+	message('<font color="#a0a000">Warning:</font> ' + msg);
+}
+
+function warningln(msg) {
+	warning(msg + '<br />\n');
+}
+
+function error(msg) {
+	message('<font color="#c00000">Error:</font> ' + msg);
+}
+
+function errorln(msg) {
+	error(msg + '<br />\n');
+}
+
 var onEvent = function(usbEvent) {
-	
 	console.log("onEvent");
 	if (usbEvent.resultCode) {
-		console.log("Error: " + usbEvent.error);
+		errorln(chrome.runtime.lastError.message + " [onEvent]");
 		return;
 	}
 
@@ -36,72 +59,110 @@ var onEvent = function(usbEvent) {
 			for (var i = 0; i < len; i++) {
 				msg += String.fromCharCode(dv.getUint8(i+2));
 			}
-			message.innerHTML += "debug: '" + msg + "'<br />\n";
+			messageln("<b>debug:</b> '" + msg + "'");
 			break;
 		}
 		case CMD_PING:
-			message.innerHTML += "command: ping<br />\n";
+			messageln("<b>ping</b>");
 			break;
 		case CMD_VERSION:
 		{
 			var version = "" + dv.getUint8(2) + "." + dv.getUint8(3);
-			message.innerHTML += "command: Version " + version + "<br />\n";
+			messageln("<b>command:</b> Version " + version);
 			break;
 		}
 		default:
-			message.innerHTML += "unknown command";
+			errorln("unknown command");
 			break;
 	}
 
 	chrome.usb.interruptTransfer(mp_device, in_transfer, onEvent);
 };
 
-var gotPermission = function(result) {
-	requestButton.style.display = 'none';
+function showMainDiv() {
+	var requestDiv = document.getElementById('requestDiv');
+	var mainDiv = document.getElementById('mainDiv');
+	requestDiv.style.display = 'none';
+	mainDiv.style.display = 'block';
+}
+
+function showRequestDiv() {
+	var requestDiv = document.getElementById('requestDiv');
+	var mainDiv = document.getElementById('mainDiv');
+	requestDiv.style.display = 'block';
+	mainDiv.style.display = 'none';
+}
+
+function sendCommand(cmd) {
+	var command = [0, cmd];
+	var info = {
+		direction: 'out',
+		endpoint: 2,
+		data: new Uint8Array(command).buffer
+	};
+
+	chrome.usb.interruptTransfer(mp_device, info, sendCompleted);
+}
+
+var setupUSBEventHandlers = function(result) {
+	showMainDiv();
+
 	console.log('App was granted the "usbDevices" permission.');
 	chrome.usb.findDevices(device_info, function(devices) {
 		console.log('Found ' + devices.length + ' devices.');
 		if (!devices || !devices.length) {
-			console.log('device not found');
+			errorln('No Mooltipass device was found.');
 			return;
 		}
 		mp_device = devices[0];
 		chrome.usb.interruptTransfer(mp_device, in_transfer, onEvent);
-
-		var version_cmd = [0x00, CMD_VERSION];
-		var version_info = {
-			direction: 'out',
-			endpoint: 2,
-			data: new Uint8Array(version_cmd).buffer
-		};
-
-		chrome.usb.interruptTransfer(mp_device, version_info, sendCompleted);
+		messageln("Mooltipass device found and set up.");
 	});
 };
 
-var permissionObj = {
-	permissions: [
-		{
-			'usbDevices': [device_info]
-		}
-	]
-};
-
-requestButton.addEventListener('click', function() {
-	console.log('Requesting permission...');
-	chrome.permissions.request(permissionObj, function(result) {
+window.addEventListener('load', function() {
+	var USBPermissions = {
+		permissions: [
+			{
+				'usbDevices': [device_info]
+			}
+		]
+	};
+	chrome.permissions.contains(USBPermissions, function(result) {
 		if (result) {
-			gotPermission();
+			setupUSBEventHandlers();
 		} else {
-			console.log('App was not granted the "usbDevices" permission.');
-			console.log(chrome.runtime.lastError);
+			showRequestDiv();
+			var requestButton = document.getElementById('requestPermissionsButton');
+			requestButton.addEventListener('click', function(ev) {
+				chrome.permissions.request(USBPermissions, function(result) {
+					if (result) {
+						setupUSBEventHandlers();
+					} else {
+						showMainDiv();
+						errorln('App was not granted the "usbDevices" persionssion');
+					}
+				});
+			});
 		}
 	});
+
+	// Set up the command button click handlers.
+	var pingButton = document.getElementById("ping");
+	pingButton.addEventListener("click", function(ev) {
+		sendCommand(CMD_PING);
+	});
+
+	var versionButton = document.getElementById("version");
+	versionButton.addEventListener("click", function(ev) {
+		sendCommand(CMD_VERSION);
+	});
+
 });
 
 function sendCompleted(usbEvent) {
 	if (chrome.runtime.lastError) {
-		console.error("sendCompleted Error:", chrome.runtime.lastError);
+		console.error("sendCompleted Error:", chrome.runtime.lastError.message);
 	}
 
 	if (usbEvent) {
@@ -110,7 +171,7 @@ function sendCompleted(usbEvent) {
 			console.log("sendCompleted Buffer:", usbEvent.data.byteLength, buf);
 		}
 		if (usbEvent.resultCode !== 0) {
-			console.error("Error writing to device", usbEvent.resultCode);
+			errorln("Error writing to device", usbEvent.resultCode);
 		}
 	}
 }

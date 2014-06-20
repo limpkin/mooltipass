eae65afc1905f6d6dfe50f35653f8341802586fa
Mathieu <mathieu@hackaday.com>
2014-06-20 14:26:44 +0200

    switch to 64 bytes packet size

--- source_code/src/USB/usb_cmd_parser.h
+++ source_code/src/USB/usb_cmd_parser.h
@@ -5,16 +5,26 @@
 #include <usb.h>
 
 /* USB mooltipass hid commands */
-#define CMD_DEBUG           0x01
-#define CMD_PING            0x02
-#define CMD_VERSION         0x03
-#define CMD_CONTEXT         0x04
-#define CMD_GET_LOGIN       0x05
-#define CMD_GET_PASSWORD    0x06
-#define CMD_SET_LOGIN       0x07
-#define CMD_SET_PASSWORD    0x08
-#define CMD_CHECK_PASSWORD  0x09
-#define CMD_ADD_CONTEXT     0x0A
+#define CMD_DEBUG               0x01
+#define CMD_PING                0x02
+#define CMD_VERSION             0x03
+#define CMD_CONTEXT             0x04
+#define CMD_GET_LOGIN           0x05
+#define CMD_GET_PASSWORD        0x06
+#define CMD_SET_LOGIN           0x07
+#define CMD_SET_PASSWORD        0x08
+#define CMD_CHECK_PASSWORD      0x09
+#define CMD_ADD_CONTEXT         0x0A
+#define CMD_EXPORT_FLASH        0x30    // resp: 0x30 packets until 0x31
+#define CMD_EXPORT_FLASH_END    0x31
+#define CMD_IMPORT_FLASH_BEGIN  0x32    // confirmed by 0x32,0x01
+#define CMD_IMPORT_FLASH        0x33    // send 4x60 byte + 1x24 byte packets, acked with 0x33,0x01
+#define CMD_IMPORT_FLASH_END    0x34
+#define CMD_EXPORT_EEPROM       0x35    // resp: 0x35 packets until 0x36
+#define CMD_EXPORT_EEPROM_END   0x36
+#define CMD_IMPORT_EEPROM_BEGIN 0x37    // confirmed by 0x37,0x01
+#define CMD_IMPORT_EEPROM       0x38    // send packet, acked with 0x38,0x01
+#define CMD_IMPORT_EEPROM_END   0x39
     
 /* Packet format defines     */
 #define HID_LEN_FIELD       0x00
--- source_code/src/defines.h
+++ source_code/src/defines.h
@@ -28,20 +28,20 @@
 #include <avr/io.h>
 #include <stdint.h>
 
+
 /**************** DEBUG PRINTFS ****************/
 // Used for smart card testing
 //#define DEBUG_SMC_SCREEN_PRINT
 #define DEBUG_SMC_USB_PRINT
 // Used for flash testing prints
-#define FLASH_TEST_DEBUG_OUTPUT_USB
-#define FLASH_TEST_DEBUG_OUTPUT_OLED
+//#define FLASH_TEST_DEBUG_OUTPUT_USB
+//#define FLASH_TEST_DEBUG_OUTPUT_OLED
 // Used for touch interface
 //#define TOUCH_DEBUG_OUTPUT_USB
 // User for general logic
 #define GENERAL_LOGIC_OUTPUT_USB
 // Used for USB comms
 #define USB_DEBUG_OUTPUT
-//#define USB_DEBUG_OUTPUT_MORE
 
 /**************** HARDWARE VERSION ****************/
 // First hardware sent to the contributors, 12/2013

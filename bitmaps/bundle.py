ecce78d61c59731949592f0465c45ed7ac8c9f8a
Josh Watts <josh+github@sroz.net>
2014-08-12 11:02:21 -0400

    bundle.py: Add --input <file> and --test

--- bitmaps/bundle.py
+++ bitmaps/bundle.py
@@ -30,6 +30,7 @@
 # Note: fonts are expected to have the word "font" in their filename
 #
 
+import os
 import sys
 from optparse import OptionParser
 from struct import *
@@ -38,11 +39,22 @@ from array import array
 parser = OptionParser(usage = '''usage: %prog [options] bitmap1 bitmap2 font1 bitmap3 font2
     note: a filename that contains word "font" will be stored as a font
           other files are stored as bitmaps''')
-parser.add_option('-o', '--output', help='name of output file', dest='output', default='bundle.img')
+parser.add_option('-o', '--output', help='name of output bundle file', dest='output', default='bundle.img')
+parser.add_option('-i', '--input', help='name of input bundle file', dest='input', default='')
+parser.add_option('-t', '--test', help='On input: list contents of input bundle. On output: Don\'t actually write to disk', dest='test_bundle', action='store_true', default=False)
+
 (options, args) = parser.parse_args()
 
 MEDIA_BITMAP = 1
 MEDIA_FONT   = 2
+
+def imageTypeToString(imageType):
+    if imageType == MEDIA_BITMAP:
+        return "bmap"
+    elif imageType == MEDIA_FONT:
+        return "font"
+    else:
+        return "unkn"
 
 def buildBundle(bundlename, files):
     data = []
@@ -81,8 +93,40 @@ def buildBundle(bundlename, files):
     bfd.close()
     print 'wrote {} bytes to {}'.format(size-reserve, bundlename)
 
+def expandBundle(bundlename, args, test_bundle=False):
+    bfd = open(bundlename, 'rb')
+
+    file_count = unpack('H', bfd.read(2))[0]
+    file_offsets = array('H')
+    file_offsets.fromfile(bfd, file_count)
+    bundle_len = os.path.getsize(bundlename)
+    file_offsets.append(bundle_len)
+
+    imageIndex = 0
+    for o in range(len(file_offsets)-1):
+        imageBegin = file_offsets[o]
+        imageEnd = file_offsets[o+1]
+        imageLen = imageEnd - imageBegin
+
+        bfd.seek(file_offsets[o])
+        imageType = unpack('H', bfd.read(2))[0]
+        imageData = bfd.read(imageLen - 2)
+        imageName = '{}_{}.img'.format(imageIndex, imageTypeToString(imageType))
+
+        print '    0x{:04x}: size {} bytes, {} {}'.format(imageBegin, imageLen, imageTypeToString(imageType), imageName)
+        if not test_bundle:
+            fd = open(imageName, 'wb')
+            fd.write(imageData)
+            fd.close
+        imageIndex += 1
+
+    bfd.close()
+
 def main():
-    buildBundle(options.output, args)
+    if len(options.input) > 0:
+        expandBundle(options.output, args, test_bundle=options.test_bundle)
+    else:
+        buildBundle(options.output, args)
 
 if __name__ == "__main__":
     main()

ec11413fc2c1774a55a30059942925a1d87bb856
Mathieu <limpkin@limpkin.fr>
2015-05-05 17:43:12 +0200

    pin entering timeout

--- source_code/src/GUI/gui_basic_functions.c
+++ source_code/src/GUI/gui_basic_functions.c
@@ -313,7 +313,7 @@ void guiMainLoop(void)
     }
     
     // If the screen just got turned on, don't call the guiScreenLoop() function
-    if ((touch_detect_result & TOUCH_PRESS_MASK) && (isScreenOnCopy != FALSE) && (screenSaverOnCopy == FALSE))
+    if ((touch_detect_result & TOUCH_PRESS_MASK) && (((isScreenOnCopy != FALSE) && (screenSaverOnCopy == FALSE)) || (getCurrentScreen() == SCREEN_DEFAULT_INSERTED_LCK)))
     {
         guiScreenLoop(touch_detect_result);
     }   
--- source_code/src/GUI/gui_pin_functions.c
+++ source_code/src/GUI/gui_pin_functions.c
@@ -100,6 +100,9 @@ RET_TYPE guiGetPinFromUser(volatile uint16_t* pin_code, uint8_t stringID)
     // Display current pin on screen
     guiDisplayPinOnPinEnteringScreen(current_pin, selected_digit);
     
+    // Arm interaction timer
+    activateTimer(TIMER_USERINT, SELECT_TIMER_DEL);
+    
     // While the user hasn't entered his pin
     while(!finished)
     {
@@ -109,6 +112,12 @@ RET_TYPE guiGetPinFromUser(volatile uint16_t* pin_code, uint8_t stringID)
         temp_rettype = touchDetectionRoutine(0);
         // Send it to the touch wheel interface logic
         temp_int8 = touchWheelIntefaceLogic(temp_rettype);
+        
+        // If something happened, rearm timer
+        if (temp_rettype != RETURN_NO_CHANGE)
+        {
+            activateTimer(TIMER_USERINT, SELECT_TIMER_DEL);
+        }
         
         // Position increment / decrement
         if (temp_int8 != 0)
@@ -125,7 +134,7 @@ RET_TYPE guiGetPinFromUser(volatile uint16_t* pin_code, uint8_t stringID)
             guiDisplayPinOnPinEnteringScreen(current_pin, selected_digit);
         }
         
-        if (isSmartCardAbsent() == RETURN_OK)
+        if ((isSmartCardAbsent() == RETURN_OK) || (hasTimerExpired(TIMER_USERINT, TRUE) == TIMER_EXPIRED))
         {
             // Smartcard removed, no reason to continue
             ret_val = RETURN_NOK;

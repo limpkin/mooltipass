a546f95e6d45a4de50a0118f15d3e61b92dafa3e
Lukas Schulze <git@mail.lukas-schulze.de>
2015-10-02 09:41:32 +0200

    todo: avoid duplicated starts of application in case of updates

--- app/js/init.js
+++ app/js/init.js
@@ -1,4 +1,5 @@
 function launch() {
+    // TODO: In case of an update, the app can be launched twice
     chrome.app.window.create('html/index.html', { 'bounds': { 'width': 800, 'height': 600 }, "resizable": false });
 
     chrome.runtime.onMessage.addListener(function(message, sender, callbackFunction) {

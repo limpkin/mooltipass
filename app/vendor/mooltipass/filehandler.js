5e81e3d9dfb261eea71e8c5474280636208c02cd
Mathieu <limpkin@limpkin.fr>
2015-10-04 12:17:53 +0200

    adding file handler

---
+++ app/vendor/mooltipass/filehandler.js
@@ -0,0 +1,58 @@
+var mooltipass = mooltipass || {};
+mooltipass.filehandler = mooltipass.memmgmt || {};
+
+mooltipass.filehandler.errorHandler = function(e) 
+{
+	var msg = '';
+
+	switch (e.code) 
+	{
+		case FileError.QUOTA_EXCEEDED_ERR:
+			msg = 'QUOTA_EXCEEDED_ERR'+e;
+			break;
+		case FileError.NOT_FOUND_ERR:
+			msg = 'NOT_FOUND_ERR'+e;
+			break;
+		case FileError.SECURITY_ERR:
+			msg = 'SECURITY_ERR z.B. Speicherplatz wurde abgelehnt.'+e;
+			break;
+		case FileError.INVALID_MODIFICATION_ERR:
+			msg = 'INVALID_MODIFICATION_ERR'+e;
+			break;
+		case FileError.INVALID_STATE_ERR:
+			msg = 'INVALID_STATE_ERR'+e;
+			break;
+		default:
+			msg = 'Unknown Error'+e;
+			break;
+	}
+
+	console.log('Writer Error: ' + msg);
+}
+
+// Ask the user to select a file and save the provided contents in it
+mooltipass.filehandler.selectAndSaveFileContents = function(name, contents, writeEndCallback) 
+{
+	chrome.fileSystem.chooseEntry({type: 'saveFile', suggestedName: name},	function(writableFileEntry) 
+																			{
+																				if(chrome.runtime.lastError)
+																				{
+																					// Something went wrong during file selection
+																					console.log("File select error: "+ chrome.runtime.lastError.message);
+																				}
+																				else
+																				{
+																					// File chosen, create writer
+																					writableFileEntry.createWriter(	function(writer) 
+																													{
+																														// Setup error and writeend call backs, start write
+																														writer.onerror = mooltipass.filehandler.errorHandler;
+																														writer.onwriteend = writeEndCallback;
+																														writer.write(contents);
+																													}, mooltipass.filehandler.errorHandler);
+																				}
+																			});
+}
+
+
+

d685e3392538ab2e37bd938bd3a88591887fc634
Lukas Schulze <git@mail.lukas-schulze.de>
2015-10-07 09:16:39 +0200

    implement keyboard gui interaction

--- app/html/js/settings.js
+++ app/html/js/settings.js
@@ -1,28 +1,56 @@
-$(function(){
-  // Load settings
-  $("#page-settings select, #page-settings input").each(function(){
-    var parameter = $(this).attr("name");
+var _s = {};
+
+_s.getKeyboardLayout = function() {
+
+}
+
+$(function() {
     mooltipass.device.interface.send({
         'command': 'getMooltipassParameter',
-        'parameter': parameter,
+        'parameter': 'keyboardLayout',
         'callbackFunction': function(_response) {
-            console.log(parameter + ':' + _response);
-            // TODO #as: show parameter in ui
+            $('#settings-keyboardLayout').val(_response.value);
         },
         'callbackParameters': null
-    });    
-  });
+    });
 
-  // Save settings on change
-  $("#page-settings select, #page-settings input").on('change keyup', function(){
-    key = $(this).attr("name");
-    if ($(this).attr('type') == 'checkbox') {
-      value = this.checked;
-    } else {
-      value = $(this).val();
-    }
+    $('#settings-keyboardLayout').change(function() {
+        mooltipass.device.interface.send({
+            'command': 'setMooltipassParameter',
+            'parameter': 'keyboardLayout',
+            'value': $(this).val(),
+            'callbackFunction': function(_response) {
+                console.log('KeyboardLayout SET:', _response);
+            },
+            'callbackParameters': null
+        });
+    });
 
-    // TODO #as: send parameters to device
-    console.log(key + ": " + value);
-  });
+    return;
+    // Load settings
+    $("#page-settings select, #page-settings input").each(function() {
+        var parameter = $(this).attr("name");
+        mooltipass.device.interface.send({
+            'command': 'getMooltipassParameter',
+            'parameter': parameter,
+            'callbackFunction': function(_response) {
+                console.log(parameter + ':' + _response);
+                // TODO #as: show parameter in ui
+            },
+            'callbackParameters': null
+        });
+    });
+
+    // Save settings on change
+    $("#page-settings select, #page-settings input").on('change keyup', function() {
+        key = $(this).attr("name");
+        if ($(this).attr('type') == 'checkbox') {
+            value = this.checked;
+        } else {
+            value = $(this).val();
+        }
+
+        // TODO #as: send parameters to device
+        console.log(key + ": " + value);
+    });
 });

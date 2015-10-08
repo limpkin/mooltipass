51278adbfa7163bb6cd4f2e300aee20dab7f8589
Lukas Schulze <github@lukas-schulze.de>
2015-10-08 12:34:05 +0200

    remove app-launcher-listener bcs multiple instances of window after dev reload

--- app/js/init.js
+++ app/js/init.js
@@ -2,10 +2,10 @@ function launch(details) {
     // TODO: As of 11/2014 this event is not fired in case of granting new permissions
     //   http://stackoverflow.com/questions/2399389/detect-chrome-extension-first-run-update#comment32831961_14957674
     if(details.reason == "install"){
-        console.log("This is a first install!");
+        //console.log("This is a first install!");
     }else if(details.reason == "update"){
-        var thisVersion = chrome.runtime.getManifest().version;
-        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+        //var thisVersion = chrome.runtime.getManifest().version;
+        //console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
     }
 
     launchWindow();
@@ -34,5 +34,5 @@ var _listenerInstalled = false;
 if(!_listenerInstalled) {
     chrome.runtime.onInstalled.addListener(launch);
     chrome.runtime.onStartup.addListener(launch);
-    chrome.app.runtime.onLaunched.addListener(launchWindow);
+    //chrome.app.runtime.onLaunched.addListener(launchWindow);
 }

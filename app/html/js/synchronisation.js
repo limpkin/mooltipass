81455a4bfef8c99ba3b294ccaf323b1640a3bfa1
Lukas Schulze <gitlab@mail.lukas-schulze.de>
2015-10-23 15:41:14 +0200

    prepare connection to synchronisation buttons

--- app/html/js/synchronisation.js
+++ app/html/js/synchronisation.js
@@ -13,7 +13,53 @@ mooltipass.ui.sync.init = function() {
 
     // Todo: Use real default values
     $("input[data-key='sync-cloud-auto']").prop("checked", true);
-}
+
+    $('#exportToFile').click(mooltipass.ui.sync.onClickExportToFile);
+    $('#importFromFile').click(mooltipass.ui.sync.onClickImportFromFile);
+
+    $('#exportToCloud').click(mooltipass.ui.sync.onClickExportToCloud);
+    $('#importFromCloud').click(mooltipass.ui.sync.onClickImportFromCloud);
+
+    $('#scanMemory').click(mooltipass.ui.sync.onClickScanMemory);
+};
+
+mooltipass.ui.sync.onClickExportToFile = function(e) {
+    e.preventDefault();
+    mooltipass.memmgmt.memoryBackupStart(true);
+};
+
+mooltipass.ui.sync.onClickImportFromFile = function(e) {
+    mooltipass.memmgmt.mergeCredentialFileToMooltipassStart();
+};
+
+mooltipass.ui.sync.onClickExportToCloud = function(e) {
+    e.preventDefault();
+    mooltipass.memmgmt.memoryBackupStart(false);
+};
+
+mooltipass.ui.sync.onClickImportFromCloud = function(e) {
+    e.preventDefault();
+    mooltipass.memmgmt.mergeSyncFSCredentialFileToMooltipassStart();
+};
+
+mooltipass.ui.sync.onClickScanMemory = function(e) {
+    e.preventDefault();
+    mooltipass.memmgmt.integrityCheckStart(mooltipass.ui.sync.progressScanMemory, mooltipass.ui.sync.callbackScanMemory);
+};
+
+mooltipass.ui.sync.progressScanMemory = function(_progress) {
+    //TODO: Implement progress bar
+    console.log('progressScanMemory(', _progress.progress, ')');
+};
+
+mooltipass.ui.sync.callbackScanMemory = function(_status) {
+    if(_status.success) {
+        mooltipass.ui.status.success($('#scanMemory'), _status.msg);
+    }
+    else {
+        mooltipass.ui.status.success($('#scanMemory'), _status.msg);
+    }
+};
 
 $(function(){
     mooltipass.ui.sync.init();

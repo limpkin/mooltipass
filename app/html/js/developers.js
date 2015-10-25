eb15b65e60daf5dfb3ac880dab837067f59a740b
Lukas Schulze <gitlab@mail.lukas-schulze.de>
2015-10-25 23:21:10 +0100

    add test methods for add/update/get credentials

--- app/html/js/developers.js
+++ app/html/js/developers.js
@@ -7,8 +7,43 @@ mooltipass.ui.developers = mooltipass.ui.developers || {};
  * Initialize function
  * triggered by mooltipass.app.init()
  */
+
+function test_getCredentials() {
+    mooltipass.device.interface.send({
+        'command': 'getCredentials',
+        'contexts': Array.prototype.slice.call(arguments),
+        'callbackFunction': function(_status) {
+            console.log('getCredentials:', _status);
+        },
+    });
+}
+
+function test_addCredentials(context, username, password) {
+    mooltipass.device.interface.send({
+        'command': 'addCredentials',
+        'context': context,
+        'username': username,
+        'password': password,
+        'callbackFunction': function(_status) {
+            console.log('updateCredentials:', _status);
+        },
+    });
+}
+
+function test_updateCredentials(context, username, password) {
+    mooltipass.device.interface.send({
+        'command': 'updateCredentials',
+        'context': context,
+        'username': username,
+        'password': password,
+        'callbackFunction': function(_status) {
+            console.log('updateCredentials:', _status);
+        },
+    });
+}
+
 mooltipass.ui.developers.init = function () {
-    $('#random-string').click(function() {
+    $('#dev-button1').click(function() {
         mooltipass.device.interface.send({
             'command': 'getRandomNumber',
             'callbackFunction': function(_status) {

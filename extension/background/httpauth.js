6110b44901a62b4002e59c0f8aa8ca6996f78491
Lukas Schulze <gitlab@mail.lukas-schulze.de>
2016-02-04 16:54:08 +0100

    improve detection of httpauth and connected mooltipass

--- extension/background/httpauth.js
+++ extension/background/httpauth.js
@@ -10,57 +10,64 @@ httpAuth.proxyUrl = null;
 
 
 httpAuth.handleRequest = function (details, callback) {
-	if(httpAuth.requestId == details.requestId || !page.tabs[details.tabId]) {
-		callback({});
-	}
-	else {
-		httpAuth.requestId = details.requestId;
-		httpAuth.pendingCallbacks.push(callback);
-		httpAuth.processPendingCallbacks(details);
-	}
+    if (httpAuth.requestId == details.requestId || !page.tabs[details.tabId]) {
+        callback({});
+    }
+    else {
+        httpAuth.requestId = details.requestId;
+        httpAuth.pendingCallbacks.push(callback);
+        httpAuth.processPendingCallbacks(details);
+    }
 }
 
-httpAuth.processPendingCallbacks = function(details) {
-	httpAuth.callback = httpAuth.pendingCallbacks.pop();
-	httpAuth.tabId = details.tabId;
-	httpAuth.url = details.url;
-	httpAuth.isProxy = details.isProxy;
+httpAuth.processPendingCallbacks = function (details) {
+    httpAuth.callback = httpAuth.pendingCallbacks.pop();
 
-	if(details.challenger){
-		httpAuth.proxyUrl = details.challenger.host;
-	}
+    // Check whether the mooltipass app is connected and directly send callback to trigger login prompt
+    if(!mooltipass.device.connectedToApp || ! mooltipass.device._app) {
+        httpAuth.callback({});
+        return;
+    }
 
-	// WORKAROUND: second parameter should be tab, but is an own object with tab-id
-	// but in background.js only tab.id is used. To get tabs we could use
-	// chrome.tabs.get(tabId, callback) <-- but what should callback be?
+    httpAuth.tabId = details.tabId;
+    httpAuth.url = details.url;
+    httpAuth.isProxy = details.isProxy;
 
-	var url = (httpAuth.isProxy && httpAuth.proxyUrl) ? httpAuth.proxyUrl : httpAuth.url;
-    console.log('httpAuth: isProxy',httpAuth.isProxy,' proxyUrl:',httpAuth.proxyUrl,'url:',httpAuth.url);
+    if (details.challenger) {
+        httpAuth.proxyUrl = details.challenger.host;
+    }
 
-	mooltipass.device.retrieveCredentials(httpAuth.loginOrShowCredentials, { "id" : details.tabId }, url, url, true);
+    // WORKAROUND: second parameter should be tab, but is an own object with tab-id
+    // but in background.js only tab.id is used. To get tabs we could use
+    // chrome.tabs.get(tabId, callback) <-- but what should callback be?
+
+    var url = (httpAuth.isProxy && httpAuth.proxyUrl) ? httpAuth.proxyUrl : httpAuth.url;
+    console.log('httpAuth: isProxy', httpAuth.isProxy, ' proxyUrl:', httpAuth.proxyUrl, 'url:', httpAuth.url);
+
+    mooltipass.device.retrieveCredentials(httpAuth.loginOrShowCredentials, {"id": details.tabId}, url, url, true);
 }
 
-httpAuth.loginOrShowCredentials = function(logins) {
-	// at least one login found --> use first to login
-	if (logins.length > 0) {
-		var url = (httpAuth.isProxy && httpAuth.proxyUrl) ? httpAuth.proxyUrl : httpAuth.url;
-		event.onHTTPAuthPopup(null, {"id": httpAuth.tabId}, {"logins": logins, "url": url});
-		//generate popup-list for HTTP Auth usernames + descriptions
+httpAuth.loginOrShowCredentials = function (logins) {
+    // at least one login found --> use first to login
+    if (logins.length > 0) {
+        var url = (httpAuth.isProxy && httpAuth.proxyUrl) ? httpAuth.proxyUrl : httpAuth.url;
+        event.onHTTPAuthPopup(null, {"id": httpAuth.tabId}, {"logins": logins, "url": url});
+        //generate popup-list for HTTP Auth usernames + descriptions
 
-		if(page.settings.autoFillAndSend) {
-			httpAuth.callback({
-				authCredentials: {
-					username: logins[0].Login,
-					password: logins[0].Password
-				}
-			});
-		}
-		else {
-			httpAuth.callback({});
-		}
-	}
-	// no logins found
-	else {
-		httpAuth.callback({});
-	}
+        if (page.settings.autoFillAndSend) {
+            httpAuth.callback({
+                authCredentials: {
+                    username: logins[0].Login,
+                    password: logins[0].Password
+                }
+            });
+        }
+        else {
+            httpAuth.callback({});
+        }
+    }
+    // no logins found
+    else {
+        httpAuth.callback({});
+    }
 }

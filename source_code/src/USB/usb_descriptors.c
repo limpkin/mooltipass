ee3847877a38032c424b0f5dd0ded5670c0545e5
Mathieu <limpkin@limpkin.fr>
2016-07-10 18:04:39 +0200

    modified descriptor to send win key on linux, added other option for key delay

--- source_code/src/USB/usb_descriptors.c
+++ source_code/src/USB/usb_descriptors.c
@@ -91,7 +91,7 @@ static const uint8_t PROGMEM keyboard_hid_report_desc[] =
     0x95, 0x06,                         //   Report Count (6),
     0x75, 0x08,                         //   Report Size (8),
     0x15, 0x00,                         //   Logical Minimum (0),
-    0x25, 0x68,                         //   Logical Maximum(104),
+    0x25, 0xff,                         //   Logical Maximum(255), was 0x68 (104) before
     0x05, 0x07,                         //   Usage Page (Key Codes),
     0x19, 0x00,                         //   Usage Minimum (0),
     0x29, 0xe7,                         //   Usage Maximum (231), was 0x68 (104) before

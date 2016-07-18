cc289817acd68141855d477d7cf95ee7a6fcad5c
Mathieu <limpkin@limpkin.fr>
2016-07-18 20:38:49 +0200

    quick bug fix

--- source_code/src/GUI/gui_screen_functions.c
+++ source_code/src/GUI/gui_screen_functions.c
@@ -937,10 +937,12 @@ RET_TYPE guiAskForConfirmation(uint8_t nb_args, confirmationText_t* text_object)
             }
 
             // Knock to approve
+            #if defined(HARDWARE_MINI_CLICK_V2)
             if (scanAndGetDoubleZTap(FALSE) != RETURN_NOK)
             {
                 input_answer = MINI_INPUT_RET_YES;
             }
+            #endif
             
             // Text scrolling
             if ((hasTimerExpired(TIMER_CAPS, TRUE) == TIMER_EXPIRED) && (nb_args > 1))

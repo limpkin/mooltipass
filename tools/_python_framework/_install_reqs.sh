887db4a9c31ac1b108d5eab36708d5c23570cb23
Mathieu <limpkin@limpkin.fr>
2016-09-04 17:13:13 +0200

    updated installer script

--- tools/_python_framework/_install_reqs.sh
+++ tools/_python_framework/_install_reqs.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 sudo apt-get install -y python python-pip python-crypto python-dev libgmp3-dev build-essential
+wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/firmwareBundlePackAndSign.py
+wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/generic_hid_device.py
+wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/mooltipass_defines.py
+wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/mooltipass_hid_device.py
+wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/mooltipass_init_proc.py
+wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/mooltipass_security_check.py
+wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/mooltipass_tool.py
+wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/publickey.bin
+wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/requirements.txt
+wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/updatefile.img
 sudo pip install -r requirements.txt
 sudo pip install seccure
+mkdir export

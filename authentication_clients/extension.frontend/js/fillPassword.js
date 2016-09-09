530b6cccc0b6b70eaec5265fc3117f82fbaf5938
Mathieu <limpkin@limpkin.fr>
2016-09-09 21:59:47 +0200

    complete repository overhaul to provide better navigation between standard and mini versions

--- authentication_clients/extension.frontend/js/fillPassword.js
+++
@@ -1,9 +0,0 @@
-function fillPassword(url, username, password) {
-	// Check if url is still the same
-	chrome.tabs.getSelected(null, function(tab){
-		console.log(tab);
-	});
-}
-
-
-mooltipass.device.onConfirmCredentials.addListener(fillPassword);

4b6582f4d2b51670e9121bc6df774006cff71f6d
Gaston Gorosterrazu <gastonius@gmail.com>
2016-10-13 13:57:09 -0300

    HTTP Login Update

--- chrome_extension/background/event.js
+++ chrome_extension/background/event.js
@@ -1,6 +1,11 @@
-var event = {};
 
-event.onMessage = function(request, sender, callback) {
+// Masquerade event var into a different variable name ( while event is not reserved, many websites use it and creates problems )
+var mooltipassEvent = {};
+
+// Keep event for backwards compatibility
+var event = mooltipassEvent;
+
+mooltipassEvent.onMessage = function(request, sender, callback) {
 	console.log("onMessage(" + request.action + ")", request);
 
 	if (request.action == 'content_script_loaded') {
@@ -9,14 +14,14 @@ event.onMessage = function(request, sender, callback) {
 		callback({response: "response from background script"});
 	}
 
-	if (request.action in event.messageHandlers) {
+	if (request.action in mooltipassEvent.messageHandlers) {
 
 		if(!sender.hasOwnProperty('tab') || sender.tab.id < 1) {
 			sender.tab = {};
 			sender.tab.id = page.currentTabId;
 		}
 
-		event.invoke(event.messageHandlers[request.action], callback, sender.tab.id, request.args);
+		mooltipassEvent.invoke(mooltipassEvent.messageHandlers[request.action], callback, sender.tab.id, request.args);
 
 		// onMessage closes channel for callback automatically
 		// if this method does not return true
@@ -37,7 +42,7 @@ event.onMessage = function(request, sender, callback) {
  * @param {bool} secondTime
  * @returns null (asynchronous)
  */
-event.invoke = function(handler, callback, senderTabId, args, secondTime) {
+mooltipassEvent.invoke = function(handler, callback, senderTabId, args, secondTime) {
 	if(senderTabId < 1) {
 		return;
 	}
@@ -69,7 +74,7 @@ event.invoke = function(handler, callback, senderTabId, args, secondTime) {
 			// using window.open()
 			if (!secondTime) {
 				window.setTimeout(function() {
-					event.invoke(handler, callback, senderTabId, args, true);
+					mooltipassEvent.invoke(handler, callback, senderTabId, args, true);
 				}, 250);
 			}
 			return;
@@ -94,35 +99,35 @@ event.invoke = function(handler, callback, senderTabId, args, secondTime) {
 }
 
 
-event.onShowAlert = function(callback, tab, message) {
+mooltipassEvent.onShowAlert = function(callback, tab, message) {
 	alert(message);
 }
 
-event.onLoadSettings = function(callback, tab) {
+mooltipassEvent.onLoadSettings = function(callback, tab) {
 	page.settings = (typeof(localStorage.settings) == 'undefined') ? {} : JSON.parse(localStorage.settings);
 	if (isFirefox) page.settings.useMoolticute = true;
     mooltipass.backend.loadSettings();
     //console.log('onLoadSettings: page.settings = ', page.settings);
 }
 
-event.onLoadKeyRing = function(callback, tab) {
-    console.log('event.onLoadKeyRing()');
+mooltipassEvent.onLoadKeyRing = function(callback, tab) {
+    console.log('mooltipassEvent.onLoadKeyRing()');
 }
 
-event.onGetSettings = function(callback, tab) {
-	event.onLoadSettings();
+mooltipassEvent.onGetSettings = function(callback, tab) {
+	mooltipassEvent.onLoadSettings();
 	var settings = page.settings;
 	settings.status = mooltipass.device._status;
 	callback({ data: settings });
 }
 
-event.onSaveSettings = function(callback, tab, settings) {
+mooltipassEvent.onSaveSettings = function(callback, tab, settings) {
 	localStorage.settings = JSON.stringify(settings);
-	event.onLoadSettings();
+	mooltipassEvent.onLoadSettings();
 }
 
-event.onGetStatus = function(callback, tab) {
-    console.log('event.onGetStatus()');
+mooltipassEvent.onGetStatus = function(callback, tab) {
+    console.log('mooltipassEvent.onGetStatus()');
 
 	browserAction.showDefault(null, tab);
     page.tabs[tab.id].errorMessage = undefined;  // XXX debug
@@ -133,32 +138,32 @@ event.onGetStatus = function(callback, tab) {
 	});
 }
 
-event.onPopStack = function(callback, tab) {
+mooltipassEvent.onPopStack = function(callback, tab) {
 	browserAction.stackPop(tab.id);
 	browserAction.show(null, tab);
 }
 
-event.onGetTabInformation = function(callback, tab) {
+mooltipassEvent.onGetTabInformation = function(callback, tab) {
 	var id = tab.id || page.currentTabId;
 
 	callback(page.tabs[id]);
 }
 
-event.onGetConnectedDatabase = function(callback, tab) {
-    console.log('event.onGetConnectedDatabase()');
+mooltipassEvent.onGetConnectedDatabase = function(callback, tab) {
+    console.log('mooltipassEvent.onGetConnectedDatabase()');
 	callback({
 		"count": 10,
 		"identifier": 'my_mp_db_id'
 	});
 }
 
-event.onRemoveCredentialsFromTabInformation = function(callback, tab) {
+mooltipassEvent.onRemoveCredentialsFromTabInformation = function(callback, tab) {
 	var id = tab.id || page.currentTabId;
 
 	page.clearCredentials(id);
 }
 
-event.onNotifyButtonClick = function(id, buttonIndex) 
+mooltipassEvent.onNotifyButtonClick = function(id, buttonIndex) 
 {
     console.log('notification',id,'button',buttonIndex,'clicked');
 	
@@ -171,53 +176,53 @@ event.onNotifyButtonClick = function(id, buttonIndex)
 	else
 	{
 		// Check notification type
-		if(event.mpUpdate[id].type == "singledomainadd")
+		if(mooltipassEvent.mpUpdate[id].type == "singledomainadd")
 		{
 			// Adding a single domain notification
 			if (buttonIndex == 0) 
 			{
 				// Blacklist
-				console.log('notification blacklist ',event.mpUpdate[id].url);
-				mooltipass.backend.blacklistUrl(event.mpUpdate[id].url);
+				console.log('notification blacklist ',mooltipassEvent.mpUpdate[id].url);
+				mooltipass.backend.blacklistUrl(mooltipassEvent.mpUpdate[id].url);
 			} 
 			else 
 			{
 			}
 		}
-		else if(event.mpUpdate[id].type == "subdomainadd")
+		else if(mooltipassEvent.mpUpdate[id].type == "subdomainadd")
 		{
 			// Adding a sub domain notification
 			if (buttonIndex == 0) 
 			{
 				// Store credentials
-				console.log('notification update with subdomain',event.mpUpdate[id].username,'on',event.mpUpdate[id].url);
-				mooltipass.device.updateCredentials(null, event.mpUpdate[id].tab, 0, event.mpUpdate[id].username, event.mpUpdate[id].password, event.mpUpdate[id].url);
+				console.log('notification update with subdomain',mooltipassEvent.mpUpdate[id].username,'on',mooltipassEvent.mpUpdate[id].url);
+				mooltipass.device.updateCredentials(null, mooltipassEvent.mpUpdate[id].tab, 0, mooltipassEvent.mpUpdate[id].username, mooltipassEvent.mpUpdate[id].password, mooltipassEvent.mpUpdate[id].url);
 			} 
 			else 
 			{
 				// Store credentials
-				console.log('notification update',event.mpUpdate[id].username,'on',event.mpUpdate[id].url2);
-				mooltipass.device.updateCredentials(null, event.mpUpdate[id].tab, 0, event.mpUpdate[id].username, event.mpUpdate[id].password, event.mpUpdate[id].url2);
+				console.log('notification update',mooltipassEvent.mpUpdate[id].username,'on',mooltipassEvent.mpUpdate[id].url2);
+				mooltipass.device.updateCredentials(null, mooltipassEvent.mpUpdate[id].tab, 0, mooltipassEvent.mpUpdate[id].username, mooltipassEvent.mpUpdate[id].password, mooltipassEvent.mpUpdate[id].url2);
 			}
 		}
-		delete event.mpUpdate[id];		
+		delete mooltipassEvent.mpUpdate[id];		
 	}
 
 	// Close notification
 	chrome.notifications.clear(id);
 }
 
-event.onNotifyClosed = function(id) {
-    delete event.mpUpdate[id];
+mooltipassEvent.onNotifyClosed = function(id) {
+    delete mooltipassEvent.mpUpdate[id];
 }
 
-chrome.notifications.onButtonClicked.addListener(event.onNotifyButtonClick);
-chrome.notifications.onClosed.addListener(event.onNotifyClosed);
+chrome.notifications.onButtonClicked.addListener(mooltipassEvent.onNotifyButtonClick);
+chrome.notifications.onClosed.addListener(mooltipassEvent.onNotifyClosed);
 
-event.notificationCount = 0;
-event.mpUpdate = {};
+mooltipassEvent.notificationCount = 0;
+mooltipassEvent.mpUpdate = {};
 
-event.isMooltipassUnlocked = function()
+mooltipassEvent.isMooltipassUnlocked = function()
 {
 	// prevents "Failed to send to device: Transfer failed" error when device is suddenly unplugged
 	if(typeof mooltipass.device._status.state == 'undefined'){
@@ -238,8 +243,8 @@ event.isMooltipassUnlocked = function()
 	}
 
 	// Increment notification count
-	event.notificationCount++;
-	var noteId = 'mpNotUnlocked.'+event.notificationCount.toString();
+	mooltipassEvent.notificationCount++;
+	var noteId = 'mpNotUnlocked.'+mooltipassEvent.notificationCount.toString();
 
 	// Check that the Mooltipass app is installed and enabled
 	if (!mooltipass.device._app || mooltipass.device._app['enabled'] !== true)
@@ -333,7 +338,7 @@ event.isMooltipassUnlocked = function()
 	return true;
 }
 
-event.onUpdateNotify = function(callback, tab, username, password, url, usernameExists, credentialsList) 
+mooltipassEvent.onUpdateNotify = function(callback, tab, username, password, url, usernameExists, credentialsList) 
 {
 	// Don't show notifications right now
 
@@ -364,14 +369,14 @@ event.onUpdateNotify = function(callback, tab, username, password, url, username
 		}
 		
 		// Check that the Mooltipass is unlocked
-		var mp_unlocked = event.isMooltipassUnlocked();
+		var mp_unlocked = mooltipassEvent.isMooltipassUnlocked();
 		
 		// Increment notification count
-		event.notificationCount++;
+		mooltipassEvent.notificationCount++;
 		
 		if(mp_unlocked && password.length > 31)
 		{		
-			var noteId = 'mpPasswordTooLong.'+ event.notificationCount.toString();
+			var noteId = 'mpPasswordTooLong.'+ mooltipassEvent.notificationCount.toString();
 			
 			chrome.notifications.create(noteId,
 				{   type: 'basic',
@@ -388,10 +393,10 @@ event.onUpdateNotify = function(callback, tab, username, password, url, username
 			if(true)
 			{
 				// Unknown user
-				var noteId = 'mpUpdate.'+event.notificationCount.toString();
+				var noteId = 'mpUpdate.'+mooltipassEvent.notificationCount.toString();
 
 				// Store our event
-				event.mpUpdate[noteId] = { tab: tab, username: username, password: password, url: domain, url2: domain, type: "singledomainadd"};
+				mooltipassEvent.mpUpdate[noteId] = { tab: tab, username: username, password: password, url: domain, url2: domain, type: "singledomainadd"};
 				
 				// Send request by default
 				mooltipass.device.updateCredentials(null, tab, 0, username, password, domain);
@@ -426,10 +431,10 @@ event.onUpdateNotify = function(callback, tab, username, password, url, username
 			else{
 
 				// Unknown user
-				var noteId = 'mpUpdate.'+event.notificationCount.toString();
+				var noteId = 'mpUpdate.'+mooltipassEvent.notificationCount.toString();
 
 				// Store our event
-				event.mpUpdate[noteId] = { tab: tab, username: username, password: password, url: domain, url2: subdomain + "." + domain, type: "subdomainadd"};
+				mooltipassEvent.mpUpdate[noteId] = { tab: tab, username: username, password: password, url: domain, url2: subdomain + "." + domain, type: "subdomainadd"};
 
 				var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 				var notification = {   
@@ -454,11 +459,11 @@ event.onUpdateNotify = function(callback, tab, username, password, url, username
 	}	
 }
 
-event.onUpdate = function(callback, tab, username, password, url, usernameExists, credentialsList) {
+mooltipassEvent.onUpdate = function(callback, tab, username, password, url, usernameExists, credentialsList) {
     mooltipass.device.updateCredentials(callback, tab, 0, username, password, url);
 }
 
-event.onLoginPopup = function(callback, tab, logins) {
+mooltipassEvent.onLoginPopup = function(callback, tab, logins) {
 	var stackData = {
 		level: 1,
 		iconType: "questionmark",
@@ -471,7 +476,7 @@ event.onLoginPopup = function(callback, tab, logins) {
 	browserAction.show(null, tab);
 }
 
-event.onHTTPAuthPopup = function(callback, tab, data) {
+mooltipassEvent.onHTTPAuthPopup = function(callback, tab, data) {
 	var stackData = {
 		level: 1,
 		iconType: "questionmark",
@@ -484,7 +489,7 @@ event.onHTTPAuthPopup = function(callback, tab, data) {
 	browserAction.show(null, tab);
 }
 
-event.onMultipleFieldsPopup = function(callback, tab) {
+mooltipassEvent.onMultipleFieldsPopup = function(callback, tab) {
 	var stackData = {
 		level: 1,
 		iconType: "normal",
@@ -496,27 +501,27 @@ event.onMultipleFieldsPopup = function(callback, tab) {
 }
 
 // all methods named in this object have to be declared BEFORE this!
-event.messageHandlers = {
-	'update': event.onUpdate,
+mooltipassEvent.messageHandlers = {
+	'update': mooltipassEvent.onUpdate,
 	'add_credentials': mooltipass.device.addCredentials,
 	'blacklist_url': mooltipass.backend.handlerBlacklistUrl,
 	'blacklistUrl': mooltipass.backend.blacklistUrl,
-	'alert': event.onShowAlert,
-	'get_connected_database': event.onGetConnectedDatabase,
-	'get_settings': event.onGetSettings,
-	'get_status': event.onGetStatus,
-	'get_tab_information': event.onGetTabInformation,
-	'load_keyring': event.onLoadKeyRing,
-	'load_settings': event.onLoadSettings,
-	'pop_stack': event.onPopStack,
-	'popup_login': event.onLoginPopup,
-	'popup_multiple-fields': event.onMultipleFieldsPopup,
-	'remove_credentials_from_tab_information': event.onRemoveCredentialsFromTabInformation,
+	'alert': mooltipassEvent.onShowAlert,
+	'get_connected_database': mooltipassEvent.onGetConnectedDatabase,
+	'get_settings': mooltipassEvent.onGetSettings,
+	'get_status': mooltipassEvent.onGetStatus,
+	'get_tab_information': mooltipassEvent.onGetTabInformation,
+	'load_keyring': mooltipassEvent.onLoadKeyRing,
+	'load_settings': mooltipassEvent.onLoadSettings,
+	'pop_stack': mooltipassEvent.onPopStack,
+	'popup_login': mooltipassEvent.onLoginPopup,
+	'popup_multiple-fields': mooltipassEvent.onMultipleFieldsPopup,
+	'remove_credentials_from_tab_information': mooltipassEvent.onRemoveCredentialsFromTabInformation,
 	'retrieve_credentials': mooltipass.device.retrieveCredentials,
 	'show_default_browseraction': browserAction.showDefault,
 	'update_credentials': mooltipass.device.updateCredentials,
-	'save_settings': event.onSaveSettings,
-	'update_notify': event.onUpdateNotify,
+	'save_settings': mooltipassEvent.onSaveSettings,
+	'update_notify': mooltipassEvent.onUpdateNotify,
 	'stack_add': browserAction.stackAdd,
 	'generate_password': mooltipass.device.generatePassword,
     'set_current_tab': page.setCurrentTab,

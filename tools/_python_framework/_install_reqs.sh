e3ed1b56dfc05a1e2e92202c5a6ab2ecbb72aa89
Mathieu <limpkin@limpkin.fr>
2016-11-13 14:33:36 +0100

    python scripts requirements update

--- tools/_python_framework/_install_reqs.sh
+++ tools/_python_framework/_install_reqs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y --force-yes install python python-pip python-crypto python-dev libgmp3-dev build-essential python-numpy python-pil python-wheel < /dev/null
+sudo apt-get -y --force-yes install python python-pip python-crypto python-dev libgmp3-dev build-essential python-numpy python-matplotlib python-wheel < /dev/null
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/firmwareBundlePackAndSign.py
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/generic_hid_device.py
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/mooltipass_defines.py
@@ -13,7 +13,8 @@ wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_pytho
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/updatefile.img
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/mooltipass_mass_prod_init_proc.py
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/png_labels.py
-sudo pip install --upgrade https://github.com/pklaus/brother_ql/archive/bef9cea.zip
 sudo pip install -r requirements.txt
 sudo pip install seccure
+sudo pip install https://github.com/pklaus/brother_ql/archive/bef9cea.zip
+sudo pip install --upgrade --no-deps https://github.com/pklaus/brother_ql/archive/bef9cea.zip
 mkdir -p export

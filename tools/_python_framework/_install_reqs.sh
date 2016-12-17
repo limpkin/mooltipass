afa5a1982680eed63ed4fd3da9cd21ea4ebe52d3
Mathieu <limpkin@limpkin.fr>
2016-12-17 18:59:48 +0100

    avrdude in pre requisite script

--- tools/_python_framework/_install_reqs.sh
+++ tools/_python_framework/_install_reqs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y --force-yes install python python-pip python-crypto python-dev libgmp3-dev build-essential python-numpy python-matplotlib python-wheel libjpeg-dev < /dev/null
+sudo apt-get -y --force-yes install python python-pip python-crypto python-dev avrdude libgmp3-dev build-essential python-numpy python-matplotlib python-wheel libjpeg-dev < /dev/null
 sudo apt-get -y --force-yes remove python-pil < /dev/null
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/firmwareBundlePackAndSign.py
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/generic_hid_device.py
@@ -8,6 +8,7 @@ wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_pytho
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/mooltipass_hid_device.py
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/mooltipass_init_proc.py
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/mooltipass_security_check.py
+wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/mooltipass_mass_prog.py
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/mooltipass_tool.py
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/publickey.bin
 wget -N https://raw.githubusercontent.com/limpkin/mooltipass/master/tools/_python_framework/requirements.txt

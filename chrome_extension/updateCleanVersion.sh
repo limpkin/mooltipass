7a213c966acf8c815509bb5efd8c74dfddb4d0ce
Gaston Gorosterrazu <gastonius@gmail.com>
2017-03-08 10:30:34 -0300

    Extension modifications for MooltiApp compatibility

--- chrome_extension/updateCleanVersion.sh
+++ chrome_extension/updateCleanVersion.sh
@@ -9,7 +9,6 @@ rm -Rf CleanVersion/*
 cp mooltipass-content.js CleanVersion/mooltipass-content.js
 cp mooltipass-content.css CleanVersion/mooltipass-content.css
 cp manifest.json CleanVersion/
-cp mooltipass-pre-jquery.js CleanVersion/
 
 cp -Rf vendor CleanVersion/
 cp -Rf popups CleanVersion/

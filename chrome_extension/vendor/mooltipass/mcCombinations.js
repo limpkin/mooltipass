d4719c023880504e0ace50b5506da1124924c6b9
Mathieu <limpkin@limpkin.fr>
2017-05-12 17:58:47 +0200

    disabling technique for steam pending more investigation

--- chrome_extension/vendor/mooltipass/mcCombinations.js
+++ chrome_extension/vendor/mooltipass/mcCombinations.js
@@ -922,13 +922,13 @@ mcCombinations.prototype.doSubmit = function doSubmit( currentForm ) {
 
 	if ( currentForm.element ) {
 		// Sites like Steam use a button outside the form:
-		if ( mpJQ('button:submit').length == 1 ) {
+		/*if ( mpJQ('button:submit').length == 1 ) {
 			// Make sure it is the button we're looking for:
 			if ( mpJQ('button:submit').find('span').length == 1 ) {
 				mpJQ('button:submit').find('span').click();
 				return;	
 			}
-		}
+		}*/
 
 		// Try to click the submit element
 		var submitButton = currentForm.element.find(':submit:visible');
